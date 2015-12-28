5f4b4025b998e689231230076af26f69c7fc8372
xuyifei <evenX86@gmail.com>
2015-12-28 10:35:58 +0800

    Update BlankDataException.php

--- src/WebGMS/Exception/BlankDataException.php
+++ src/WebGMS/Exception/BlankDataException.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: yuhuitao
- * Date: 13-2-18
- * Time: 下午5:29
- * To change this template use File | Settings | File Templates.
- */
+
 namespace WebGMS\Exception;
 class BlankDataException extends  \RuntimeException {}
